81b863af19d120f97a0b76aae38bf4a024df41b6
cedarmora <cedarmora@gmail.com>
2019-12-20 15:51:38 -0800

    Create server test

--- web/app/views.py
+++ web/app/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import jsonify
 from app import app
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return jsonify('{"test": "hey"}')
