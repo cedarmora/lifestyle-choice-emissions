7fd5d6d0fa66cccae354654d71953f8b932e3dda
cedarmora <cedarmora@gmail.com>
2020-01-03 16:24:32 -0800

    Trying React component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Chart from './Chart';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Chart />
         <p>
           Lifestyle Choice Emissions
         </p>
---
+++ frontend/src/Chart.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Chart extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <p>Data: </p>
+    )
+  }
+}
+
+export default Chart
