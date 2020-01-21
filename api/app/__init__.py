63863b3b5ff720f7f92b82273ea6b76a3ccf68e6
cedarmora <cedarmora@gmail.com>
2020-01-21 14:15:05 -0800

    No CORS error! But it's not very secure hmmm

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+# from flask_cors import CORS
+import os
 
 # Initialize app
 app = Flask(__name__)
+# frontend_url = os.environ.get('FRONTEND_URL')
+# cors = CORS(app, resources={r"/*": {"origins": frontend_url}})
+
 
 # Load views
 from app import views
