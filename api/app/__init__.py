f70370a1c9cc1c9af12d25de36a6447a6a7e303c
cedarmora <cedarmora@gmail.com>
2020-01-21 14:39:37 -0800

    Remove unnecessary flask CORS stuff

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,11 +1,8 @@
 from flask import Flask
-# from flask_cors import CORS
 import os
 
 # Initialize app
 app = Flask(__name__)
-# frontend_url = os.environ.get('FRONTEND_URL')
-# cors = CORS(app, resources={r"/*": {"origins": frontend_url}})
 
 
 # Load views
